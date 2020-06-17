beb47eb8d9f4ce6330c17cb86c1ed0c60feb6c90
Matthew Gilligan <mcgilligan97@gmail.com>
2020-06-17 12:02:26 +0100

    imports and class definitions

--- game.py
+++ game.py
@@ -1,1 +1,9 @@
 import random
+
+suits = ('Hearts', 'Diamond', 'Spades', 'Clubs')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', ' Seven',
+         'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King')
+values = pass
+
+
+playing = True
