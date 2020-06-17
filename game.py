b851aa1475f8a633b061c4b2346403f81504fe05
Matthew Gilligan <mcgilligan97@gmail.com>
2020-06-17 12:59:13 +0100

    final gameply script

--- game.py
+++ game.py
@@ -89,7 +89,7 @@ def hit(deck, hand):
   hand.adjust_for_ace()
 
 
-def (hit_or_stand):
+def hit_or_stand():
   global playing
 
   while True:
@@ -130,3 +130,57 @@ def dealer_wins(player, dealer, chips):
 
 def push(player, deale):
   print("Dealer and player tie! PUSH")
+
+
+while True:
+  print("WELCOME TO BLACKJACK")
+  deck = Deck()
+  deck.shuffle()
+
+  player_hand = Hand()
+  player_hand.add_card(deck.deal())
+  player_hand.add_card(deck.deal())
+
+  dealer_hand = Hand()
+  dealer_hand.add_card(deck.deal())
+  dealer_hand.add_card(deck.deal())
+
+  player_chips = Chips()
+
+  take_bet(player_chips)
+
+  show_some(player_hand, dealer_hand)
+
+  while playing:
+    hit_or_stand(deck, player_hand)
+
+    show_some(player_hand, dealer_hand)
+
+    if player_hand.value > 21:
+      player_busts(player_hand, dealer_hand, player_chips)
+      break
+
+  if player_hand.value <= 21:
+    while dealer_hand.value < 17:
+      hit(deck, dealer_hand)
+
+    show_all(player_hand, dealer_hand)
+
+    if dealer_hand.value > 21:
+      dealer_busts(player_hand, dealer_hand, player_chips)
+    elif dealer_hand.value > player_hand.value:
+      dealer_wins(player_hand, dealer_hand, player_chips)
+    elif dealer_hand.value < player_hand.value:
+      player_wins(player_hand, dealer_hand, player_chips)
+    else:
+      push(player_hand, dealer_hand)
+
+  print("\n Player total chips are at: {}".format(player_chips.total))
+  new_game = input("Would you like to play another hand? Y/n")
+
+  if new_game[0].lower() == 'y':
+    playing = True
+    continue
+  else:
+    print("Thank you for playing!")
+    break
